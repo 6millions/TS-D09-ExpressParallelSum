0bdb0f9d84c811a43b4946d2d8d38939c0d72b73
6millions <kittichai.s@ic-connection.com>
2024-07-16 23:09:39 +0700

    add initial controller and route

---
+++ src/routes/sumRoutes.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+
+
+const routes = express.Router();
+
+// create controller
+
+// mapping route with controller 
+
+
+
+export default routes;
