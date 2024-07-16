1f7f784c476f84de4b70279854e740db1c20515c
6millions <kittichai.s@ic-connection.com>
2024-07-17 00:19:22 +0700

    add more model

---
+++ src/controllers/sumController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+// import model
+import { SumRequest } from "../models/sumRequest";
+
+// create controller class
+export class SumController {
+
+    constructor() {
+
+
+    }
+
+    // post logic to calculate the summary of array.
+    public getSumRequest = (req: Request, res: Response): void => {
+
+
+    }
+
+
+}
---
+++ src/models/sumRequest.ts
@@ -0,0 +1,12 @@
+export class SumRequest {
+
+    private _NumberOfArray: number;
+    private _LengthOfArray: number;
+
+    constructor(numberOfArray: number, lengthOfArray: number) {
+        this._NumberOfArray = numberOfArray;
+        this._LengthOfArray = lengthOfArray;
+
+    }
+
+}
